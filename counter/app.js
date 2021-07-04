292400568cf9dd186771a5d87006057d52732ca6
mahirlock <86570698+mahirlock@users.noreply.github.com>
2021-07-04 20:22:17 +0200

    Add files via upload

---
+++ counter/app.js
@@ -0,0 +1,28 @@
+let count = 0;
+
+let value = document.getElementById('value');
+let btns = document.querySelectorAll('.btn');
+
+btns.forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+        let styles = e.currentTarget.classList;
+        
+        if (styles.contains('decrease')) {
+            count--;
+        } else if (styles.contains('increase')) {
+            count++;
+        } else {
+            count = 0;
+        }
+
+        if (count > 0) {
+            value.style.color = 'green';
+        } else if (count < 0) {
+            value.style.color = 'red';
+        } else {
+            value.style.color = 'black';
+        }
+
+        value.textContent = count;
+    })
+})
